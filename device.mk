d33938613eb87b9001a902747bfdf2700a495eba
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-03 03:08:12 +0530

    vermeer: Show "Turbo charging" instead of "Charging rapidly"
    Like MIUI

--- device.mk
+++ device.mk
@@ -447,7 +447,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay
 
 # Perf
 PRODUCT_PACKAGES += \
