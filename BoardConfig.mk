dd509bdefb3ad55d40e1546a095a1d74b8043e93
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-09-28 13:30:22 -0500

    vermeer: We will use a kernel prebuilt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,12 +5,24 @@
 #
 
 DEVICE_PATH := device/xiaomi/vermeer
+KERNEL_PATH := device/xiaomi/vermeer-kernel
 
 # Inherit from sm8550-common
 include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 
 # Display
 TARGET_SCREEN_DENSITY := 530
+
+# DTB
+BOARD_USES_DT := true
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtbs
+BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbs/dtbo.img
+
+TARGET_NO_KERNEL_OVERRIDE := true
+
+TARGET_KERNEL_SOURCE := $
+
+TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/Image
 
 # Kernel Modules
 device_second_stage_modules := \
