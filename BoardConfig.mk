5220c8a714ac84809c57c3ecc62ad06a8fc215d7
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-09-29 20:24:03 -0500

    vermeer: Add the kernel properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,11 +18,45 @@ BOARD_USES_DT := true
 BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtbs
 BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbs/dtbo.img
 
+# Kernel
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image
+
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+
+TARGET_FORCE_PREBUILT_KERNEL := true
+
+BOARD_KERNEL_CMDLINE := \
+    kasan=off \
+    disable_dma32=on \
+    rcu_nocbs=all \
+    rcutree.enable_rcu_lazy=1 \
+    mtdoops.fingerprint=$(LINEAGE_VERSION)
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.init_fatal_reboot_target=recovery
+
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+
+# Prevent overwriting of the already built kernel
 TARGET_NO_KERNEL_OVERRIDE := true
 
-TARGET_KERNEL_SOURCE := $
+# Specifies the path to the kernel headers to be used.
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 
 TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/Image
+PRODUCT_COPY_FILES += \
+    $(TARGET_PREBUILT_KERNEL):kernel
 
 # Kernel Modules
 device_second_stage_modules := \
