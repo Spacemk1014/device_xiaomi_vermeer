284c553c843b0415cdeaffbf57f0c4c212eb1d7c
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-10-01 21:25:36 -0500

    vermeer: Add sepolicy_vndr and system qcom to dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,6 @@ rm -rf device/xiaomi/vermeer-kernel
 git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_vermeer-kernel.git device/xiaomi/vermeer-kernel 
 
 # Camera 
-rm -rf vendor/xiaomi/camera 
 git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm8550 vendor/xiaomi/camera 
 
 # Common Tree 
@@ -21,3 +20,10 @@ git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_sepolic
 
 # Hardware Qcom
 git clone --depth=1 https://github.com/Spacemk1014/android_hardware_qcom-caf_sm8550_gps.git hardware/qcom-caf/sm8550/gps
+
+# Sepolicy_vndr
+rm -rf device/qcom/sepolicy_vndr/sm8550
+git clone --depth=1 https://github.com/Spacemk1014/android_device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8550
+
+# System Qcom
+git clone --depth=1 https://github.com/Spacemk1014/android_system_qcom.git system/qcom
